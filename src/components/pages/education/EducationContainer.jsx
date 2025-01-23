97d70ebd509379cf17165f661804916f765df03c
Nicolas <nicolasboscasso@gmail.com>
2025-01-23 16:43:17 -0300

    added education.js

--- src/components/pages/education/EducationContainer.jsx
+++ src/components/pages/education/EducationContainer.jsx
@@ -1,63 +1,7 @@
 import "./education.scss";
+import educationItems from "./educationData";
 
 const EducationContainer = () => {
-  const educationItems = [
-    {
-      title: "Higher Technical Degree in Programming",
-      school: "Teclab",
-      date: "August 2024 - present",
-    },
-    {
-      title: "React.js",
-      school: "CoderHouse",
-      date: "September 2024 - November 2024",
-      pdfLink:
-        "https://res.cloudinary.com/dl73hi4ir/image/upload/v1737660441/ReactJs_oi1pvd.png",
-    },
-    {
-      title: "Backend Programming",
-      school: "CoderHouse",
-      date: "March 2024 - September 2024",
-      pdfLink:
-        "https://res.cloudinary.com/dl73hi4ir/image/upload/v1731900299/backend-certificate_bsey4g.png",
-    },
-    {
-      title: "JavaScript",
-      school: "CoderHouse",
-      date: "December 2023 - March 2024",
-      pdfLink:
-        "https://res.cloudinary.com/dl73hi4ir/image/upload/v1731900261/javascript-certificate_ubtkhx.png",
-    },
-    {
-      title: "Python",
-      school: "CoderHouse",
-      date: "August 2022 - November 2022",
-      pdfLink:
-        "https://res.cloudinary.com/dl73hi4ir/image/upload/v1731900258/python-certificate_htcmgz.png",
-    },
-    {
-      title: "SQL",
-      school: "CoderHouse",
-      date: "March 2022 - June 2022",
-      pdfLink:
-        "https://res.cloudinary.com/dl73hi4ir/image/upload/v1731900256/sql-certificate_cmzrnw.png",
-    },
-    {
-      title: "Web Developer",
-      school: "CoderHouse",
-      date: "June 2021 - September 2021",
-      pdfLink:
-        "https://res.cloudinary.com/dl73hi4ir/image/upload/v1731900259/web-dev-certificate_fxsnjk.png",
-    },
-    {
-      title: "Language",
-      school: "EF Standar English Test - English C2 (Proficient)",
-      date: "Jan 2024",
-      pdfLink:
-        "http://www.efset.org/cert/GvMKoR",
-    },
-  ];
-
   return (
     <section className="education-background education-section">
       <div className="container d-flex flex-column align-items-center">
